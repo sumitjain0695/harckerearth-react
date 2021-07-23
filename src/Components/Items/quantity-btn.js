39bc0ebb2d35b1e6cd6b2172c37dbd736251df09
Sumit <sumit.jain@krazybee.com>
2021-07-24 02:43:03 +0530

    style refactoring and data added

--- src/Components/Items/quantity-btn.js
+++ src/Components/Items/quantity-btn.js
@@ -4,7 +4,7 @@ function QuantityButton(props) {
   return (
     <div className="btn-container">
       <div>
-        <span className="btn-left">-</span>
+        <span className="btn-left">−</span>
         <p className="btn-mid"> {4332}</p>
         <span className="btn-right">+</span>
       </div>
