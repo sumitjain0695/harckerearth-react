622ac7fddc990d4d489bede66cd4020dcf27a532
Sumit <sumit.jain@krazybee.com>
2021-07-24 02:04:26 +0530

    responsive class changes

--- src/Components/baselayout.js
+++ src/Components/baselayout.js
@@ -9,14 +9,13 @@ function Baselayout(props) {
       <div className="order-summary">
         <span className="back-icon">&#8249;</span> <span>Order Summary</span>
       </div>
-      <hr className="separator" />
-
       <div className="segments">
-        <div className="left-segment">
-          <Itemstable />
-        </div>
         <div className="right-segment">
           <TotalCalc />
+        </div>
+
+        <div className="left-segment">
+          <Itemstable />
         </div>
       </div>
     </div>
